a320c9cdbf1beb4922be4d7308b19ba8c84a0012
rsivaseshu <rsivaseshu@gmail.com>
2022-12-26 19:11:50 +0530

    backend configuration update

--- sept2022/finance/main.tf
+++ sept2022/finance/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "rssr-tf-state"
+    key    = "finance/infra"
+    region = "ap-south-1"
+    profile = "rsiva"
+  }
 }
 
 
