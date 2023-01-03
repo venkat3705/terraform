c792f4c8e9f540747f2ac026b983a24e43ced9f3
venkat3705 <106795182+venkat3705@users.noreply.github.com>
2023-01-03 20:14:27 +0530

    Update variables.tf

--- sept2022/aws-s3/variables.tf
+++ sept2022/aws-s3/variables.tf
@@ -2,11 +2,11 @@
 variable "bucket_name" {
   type        = string
   description = "Please enter your bucket name"
-  default     = "rssr-tf-sample-bucket-1"
+  default     = "mvrr-tf-sample-bucket-1"
 }
 
 variable "owner" {
   type        = string
   description = "Enter owner name"
-  default = "Rsiva"
+  default = "administrator"
 }
